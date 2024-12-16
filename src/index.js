60d5cb2b5c96cc312a89a184b099bd50590efc68
Deborah964 <fiankobeadeborah@gmail.com>
2024-12-16 11:34:57 +0000

    my homework9 file

---
+++ src/index.js
@@ -0,0 +1,31 @@
+let go=document.getElementById("line1");
+go.style.backgroundColor=("blue");
+
+
+let am=document.getElementById("line2");
+am.style.backgroundColor=("grey");
+am.style.color=("black");
+am.innerHTML=("I know now what the DOM is 🙌");
+
+let is=document.getElementById("line3");
+is.style.backgroundColor=("red");
+is.style.color=("black");
+is.innerHTML=("I know how to access/ select elements in a DOM🐒");
+
+
+let me=document.getElementById("line4");
+me.style.backgroundColor=("orange");
+me.style.color=("black");
+me.innerHTML=("I know  how to use the style methods to change the styles of an element in the DOM🐾");
+
+
+let it=document.getElementById("line5");
+it.style.backgroundColor=("green");
+it.style.color=("black");
+it.innerHTML=("I know how the styling properties differ in JS from CSS☑");
+
+
+let to=document.getElementById("line6");
+to.style.backgroundColor=("black");
+to.style.color=("white");
+to.innerHTML=("Yes I did it!!! I manipulated the DOM and styled the elements🥰🥰🥰🥰🥰🥰");
